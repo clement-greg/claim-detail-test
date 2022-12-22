571bf6fd965ead359ee8ac331fca92ba1388e277
Greg Clement <g.clement@upkeeplabs.com>
2022-12-22 08:52:08 -0700

    a

--- src/app/test-camera/test-camera.component.ts
+++ src/app/test-camera/test-camera.component.ts
@@ -1,36 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, forwardRef, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => TestCameraComponent),
+  multi: true
+};
+
+const noop = () => { };
 
 @Component({
   selector: 'app-test-camera',
   templateUrl: './test-camera.component.html',
-  styleUrls: ['./test-camera.component.css']
+  styleUrls: ['./test-camera.component.css'],
+  providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR]
 })
-export class TestCameraComponent implements OnInit {
+export class TestCameraComponent implements OnInit, ControlValueAccessor {
 
   streamStarted = false;
+  cameraRunning = false;
+  private onTouchedCallback: () => void = noop;
+  private onChangeCallback: (_: any) => void = noop;
 
   constructor() { }
+  writeValue(obj: any): void {
+    //throw new Error('Method not implemented.');
+  }
+  registerOnChange(fn: any): void {
+    this.onChangeCallback = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouchedCallback = fn;
+  }
+  // setDisabledState?(isDisabled: boolean): void {
+  //   throw new Error('Method not implemented.');
+  // }
 
   ngOnInit(): void {
   }
 
-  async test() {
-    this.startStream();
+  async startCamera() {
+    this.cameraRunning = true;
+    setTimeout(()=> {
+
+      this.startStream();
+    });
   }
 
   async startStream() {
+    console.log({width: window.innerWidth, height: window.innerHeight})
     const constraints = {
       video: {
-        width: {
-          min: 1280,
-          ideal: 1920,
-          max: 2560,
-        },
-        height: {
-          min: 720,
-          ideal: 1080,
-          max: 1440
-        },
+        width: screen.width,
+        height: screen.height,
         facingMode: 'environment'
       }
     };
@@ -53,10 +75,20 @@ export class TestCameraComponent implements OnInit {
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
     canvas.getContext('2d').drawImage(video, 0, 0);
-    console.log(canvas.toDataURL('image/webp'));
+    //console.log(canvas.toDataURL('image/webp'));
+    this.onChangeCallback(canvas.toDataURL('image/webp'));
 
-    (document.getElementById('screenshot') as any).src = canvas.toDataURL('image/webp');
+    //(document.getElementById('screenshot') as any).src = canvas.toDataURL('image/webp');
     video.pause();
+    setTimeout(()=> this.cameraRunning = false, 500);
+    //this.cameraRunning = false;
+  }
+
+  closeCamera() {
+    
+    const video = document.querySelector('video');
+    video.pause();
+    this.cameraRunning = false;
   }
 
 }
